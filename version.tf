63b7c5972c35006a1949729e77368e9340dfb590
Rajendra K <james-trainer75@outlook.com>
2022-04-01 18:05:02 +0530

    Working code

--- version.tf
+++ version.tf
@@ -3,15 +3,21 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "4.5.0"
-   
+
     }
     random = {
       source  = "hashicorp/random"
       version = "3.1.2"
     }
   }
-    backend "s3" {
-     }
+  backend "s3" {
+    bucket         = "terraform-aws-1422"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform"
+    encrypt        = true
+    profile        = "root"
+  }
 }
 
 provider "aws" {
